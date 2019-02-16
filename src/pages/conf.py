84478172142add786e873bfbb450f9e316e66b34
Михаил Красильников <m.krasilnikov@yandex.ru>
2019-02-16 10:18:29 +0300

    Подключаемые файлы не должны рассматриваться как исходники

--- src/pages/conf.py
+++ src/pages/conf.py
@@ -74,7 +74,9 @@ language = 'ru'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = []
+exclude_patterns = [
+    '_include/*',
+]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
